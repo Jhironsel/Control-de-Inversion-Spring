b7c6c0ce5766ec0efe0d3cd757ea0bfa97a41d51
Jhironsel Diaz A. <jhironseld@gmail.com>
2023-12-19 18:45:01 -0400

    Hasta aqui el concepto de IoC...

--- src/main/java/sur/softsurena/IOC/UsoIoC.java
+++ src/main/java/sur/softsurena/IOC/UsoIoC.java
@@ -5,23 +5,31 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class UsoIoC {
 
     public static void main(String[] args) {
-        /*Persona empleado = new Ingeniero();
-        System.out.println(empleado.getOficio());*/
-        //1. cargar el archivo xml.
+        /*
+        Esta es la forma tradicional de hacer instancias de nuestras clases.
+        La forma en como se usaba. 
+        Persona empleado = new Ingeniero();
+        System.out.println(empleado.getOficio());
+        */
 
-        try (ClassPathXmlApplicationContext contexto
+        //1. cargar el archivo xml.
+        try (ClassPathXmlApplicationContext contexto //La variable context 
                 = new ClassPathXmlApplicationContext(
-                        "contextoApp.xml"
+                        "contextoApp.xml" //Aqui esta el archivo xml.
                 )) {
             //2. Pedir el bean
-            Persona silvia = contexto.getBean(
+            Persona silvia = contexto.getBean( //Aqui pedimos un bean a netbeans.
                     "miEmpleado",
                     Persona.class
             );
-
-            //3. Utilizar el bean
+            //Fijarse que el tipo de la variable silvia es la interface.
+            //Los parameros que recibe es el nombre de bean y La interface con su atributo class.
+            
+            //3. Utilizar el bean que hemos creado.
             System.out.println(silvia.getClass().getCanonicalName());
             System.out.println("Su tarea es: " + silvia.getOficio());
+            System.out.println("Que sale aqui: "+Persona.class);
+            System.out.println("Que sale aqui: "+Jefe.class);
         }
 
     }
