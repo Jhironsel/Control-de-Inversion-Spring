b60dc963886a2541f35f34b52ec69f3f02c64174
Jhironsel Diaz A <jhironseld@gmail.com>
2023-12-22 01:33:32 -0400

    Proyecto base
    Ahora vamos a continuar con las anotaciones en Spring.

--- src/main/java/sur/softsurena/IOC/IInforme.java
+++
@@ -1,6 +0,0 @@
-package sur.softsurena.IOC;
-
-public interface IInforme {
-    //Segundo parte.
-    String getInforme();
-}
--- src/main/java/sur/softsurena/IOC/IPersona.java
+++
@@ -1,6 +0,0 @@
-package sur.softsurena.IOC;
-
-public interface IPersona extends IInforme {
-    //Primera Parte.
-    String getOficio();
-}
